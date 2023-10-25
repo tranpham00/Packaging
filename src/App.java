fdb98b116d9a35e4a3ebf714e8852e88d9f09a89
tran pham <trannpham.05@gmail.com>
2023-10-25 10:52:59 -0500

    added more to main

--- src/App.java
+++ src/App.java
@@ -5,5 +5,8 @@ public class App {
         Stack<String> stack = new Stack<String>();
         stack.push("hello world");
         System.out.println(stack.peek());
+        System.out.println(stack.isEmpty());
+        System.out.println(stack.pop());
+        System.out.println(stack.isEmpty());
     }
 }
